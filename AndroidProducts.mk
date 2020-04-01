6cfb3238241f6a676db3bf9f90b5a63dedaea785
Tailanbr <51462858+Tailanbr@users.noreply.github.com>
2020-04-01 02:16:47 -0300

    start stag

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -15,9 +15,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_river.mk
+    $(LOCAL_DIR)/stag_river.mk
 
 COMMON_LUNCH_CHOICES := \
-    lineage_river-eng \
-    lineage_river-user \
-    lineage_river-userdebug
+    stag_river-eng \
+    stag_river-user \
+    stag_river-userdebug
